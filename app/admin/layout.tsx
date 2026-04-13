edc8e80eea5e83e6c4169af3be62c6b3e7c49577
jayson316 <jaysontyler@gmail.com>
2026-04-13 20:32:01 +0000

    Simple admin layout

---
+++ app/admin/layout.tsx
@@ -0,0 +1,5 @@
+"use client";
+import { AuthProvider } from "@/lib/AuthContext";
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return <AuthProvider>{children}</AuthProvider>;
+}
